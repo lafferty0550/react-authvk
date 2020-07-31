3c71abd2236b4f15b2d32b20c973e5a12a88c39b
Ilya Mashinin <lafferty0550@gmail.com>
2020-07-31 21:47:06 +0300

    Updates

---
+++ precache-manifest.3fc3cf9e38616445a1cffb3c6ffb8f1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1820824271aa961a107627cd54162d",
+    "url": "/react-authvk/index.html"
+  },
+  {
+    "revision": "db6402172200e12a0fad",
+    "url": "/react-authvk/static/css/main.88d456d1.chunk.css"
+  },
+  {
+    "revision": "6fd2d593f8b2556ca1fe",
+    "url": "/react-authvk/static/js/2.b8f0aef3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-authvk/static/js/2.b8f0aef3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db6402172200e12a0fad",
+    "url": "/react-authvk/static/js/main.e4e2f55d.chunk.js"
+  },
+  {
+    "revision": "7355d55130b513d598ee",
+    "url": "/react-authvk/static/js/runtime-main.b85e0fb9.js"
+  }
+]);
